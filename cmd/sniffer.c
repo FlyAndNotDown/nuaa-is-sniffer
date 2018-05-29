6302ad69811623851ff46a090909681290cd1c68
John Kindem <461425614@qq.com>
2018-05-29 18:36:07 +0800

    * add: 测试

--- cmd/sniffer.c
+++ cmd/sniffer.c
@@ -1,5 +1,7 @@
 #include "./sniffer.h"
 
+etherpacket ep;
+
 int main(int argc, char *argv[]) {
     return 0;
 }
--- cmd/sniffer.h
+++ cmd/sniffer.h
@@ -10,6 +10,23 @@
 #include <net/if.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
 #include <netinet/tcp.h>
 #include <netinet/if_ether.h>
 #include <stdlib.h>
+
+int openintf(char *);
+int read_tcp(int);
+int filter(void);
+int print_header(void);
+int print_data(int, char *);
+char *hostlookup(unsigned long int);
+void clear_victim(void);
+void cleanup(int);
+
+typedef struct {
+    struct ethhdr eth;
+    struct iphdr ip;
+    struct tcphdr tcp;
+    char buffer[8192];
+} etherpacket;
